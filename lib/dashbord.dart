5ecbc6313ea7e125eb3c80e825c2172e290a7c81
arunkumar139 <blacklover.cho@gmail.com>
2023-08-20 19:58:27 +0530

    update dasboard claas and mention in main

---
+++ lib/dashbord.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dasboard extends StatefulWidget {
+  const Dasboard({super.key});
+
+  @override
+  State<Dasboard> createState() => _DasboardState();
+}
+
+class _DasboardState extends State<Dasboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
